97da08fa6b04167238b8be35d68571a06f1240af
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2023-11-20 17:23:34 +0530

    Learned `Input` and `Output` streams using `System.Console`

---
+++ Learning/C_Sharp/Program.cs
@@ -0,0 +1,41 @@
+﻿
+namespace C_Sarp
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Providing menue and instructions
+            Console.WriteLine("[M] Write the Name of Project to Run\n");
+
+            Console.WriteLine("[A] Available Projects:");
+            Console.WriteLine("[ ] [0] -> ConsoleAndIO (Input and output handeling).");
+
+            // Take input from user
+            Console.Write("[Y] Your Choice:");
+            string? user_input = Console.ReadLine();
+
+            int ProjectToRun = 0;
+            if (user_input != null)
+            {
+                ProjectToRun = int.Parse( user_input );
+            }
+
+            if ( ProjectToRun == 0)
+            {
+                Console.WriteLine("\n[=] Running ConsoleAndIO.Example().");
+                ConsoleAndIO.Example();
+            }
+            else
+            {
+                Console.WriteLine($"[!] Invalid Choice {ProjectToRun} is not valid Project.");
+            }
+
+            Console.WriteLine($"[=] EOP End of project.\n");
+            Console.WriteLine($"[E] Press any key to Exit.");
+            // Wait Before Ending
+            Console.ReadKey();
+        }
+    }
+}
+
