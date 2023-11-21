86e1dc9fad853fa4725060c81405d440a7817303
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2023-11-21 14:10:10 +0530

    Changed configuration `<ImplicitUsings>`

--- Learning/C_Sharp/Program.cs
+++ Learning/C_Sharp/Program.cs
@@ -1,15 +1,22 @@
-﻿
+﻿/* if you import libraries like (using System;) you can only use it within file.
+ * But if you import it as (global using global::System;) you can use it globally throughout the project.
+ * 
+ * Note that Visual studio projects can do it on its own using <project_name>.GlobalUsing.g.cs
+ * You can turn this behaviour off in cs_project-file. by using [ <ImplicitUsings>disable</ImplicitUsings>]
+ */
+global using global::System;
+
 namespace C_Sharp
 {
     class Program
     {
         static void Main()
         {
-            // Providing menue and instructions
+            // Providing menu and instructions
             Console.WriteLine("[M] Write the Name of Project to Run\n");
 
             Console.WriteLine("[A] Available Projects:");
-            Console.WriteLine("[ ] [0] -> ConsoleAndIO (Input and output handeling).");
+            Console.WriteLine("[ ] [0] -> ConsoleAndIO (Input and output handling).");
 
             // Take input from user
             Console.Write("[Y] Your Choice:");
