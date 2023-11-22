f9fcbfc734169372954949531fb34b271911d976
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2023-11-22 14:30:59 +0530

    added core functions.

---
+++ Learning/C_Sharp/Projects/Calculator/Core.cs
@@ -0,0 +1,52 @@
+﻿
+
+namespace Calculator
+{
+    public class Core
+    {
+
+        public static int Add(int number1, int number2) => number1 + number2;
+        public static long Add(long number1, long number2) => number1 + number2;
+        public static float Add(float number1, float number2) => number1 + number2;
+        public static double Add(double number1, double number2) => number1 + number2;
+
+
+        public static int Sub(int number1, int number2) => number1 - number2;
+        public static long Sub(long number1, long number2) => number1 - number2;
+        public static float Sub(float number1, float number2) => number1 - number2;
+        public static double Sub(double number1, double number2) => number1 - number2;
+
+
+        public static int Mul(int number1, int number2) => number1 * number2;
+        public static long Mul(long number1, long number2) => number1 * number2;
+        public static float Mul(float number1, float number2) => number1 * number2;
+        public static double Mul(double number1, double number2) => number1 * number2;
+
+
+        public static float Div(int number1, int number2)
+        {
+            if (number2 == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
+            return number1 / number2;
+        }
+        public static float Div(float number1, float number2)
+        {
+            if (number2 == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
+            return number1 / number2;
+        }
+        public static double Div(double number1, double number2)
+        {
+            if (number2 == 0)
+            {
+                throw new ArgumentException("Cannot divide by zero.");
+            }
+            return number1 / number2;
+        }
+
+    }
+}
