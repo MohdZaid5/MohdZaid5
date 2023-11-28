5f6383299c90533aab5bf23fc1726de22d3ef4a8
Mohd Zaid <101700541+MohdZaid5@users.noreply.github.com>
2023-11-28 14:08:33 +0530

    Revert "added `New Configurations`"
    This reverts commit 748fe9c35b13b81b3ef5dc636b689c2f57415e96.

--- Learning/C_Sharp/Projects/Calculator/Program.cs
+++ Learning/C_Sharp/Projects/Calculator/Program.cs
@@ -1,9 +1,5 @@
 ﻿
 global using global::System;
-global using global::System.Collections.Generic;
-
-using BenchmarkDotNet.Running;
-using System.Diagnostics;
 
 namespace Calculator
 {
@@ -13,93 +9,47 @@ namespace Calculator
 
         public static void Main()
         {
-            // Run the main program
-            RunProgram();
-
-            // Run benchmarks if the "BENCHMARK" conditional compilation symbol is defined
-            RunBenchmarks();
-        }
-
-        // Method to run benchmarks (conditionally compiled)
-        [Conditional("BENCHMARK")]
-        public static void RunBenchmarks()
-        {
-            // Run benchmarks using BenchmarkRunner for the FactorAlgorithmBenchmarks class
-            BenchmarkRunner.Run<FactorAlgorithmBenchmarks>();
-            // Wait for a key press before closing the console (useful for observing benchmark results)
-            Console.ReadKey();
-        }
-
-        // Method to run the main program (conditionally compiled for both "DEBUG" and "RELEASE")
-        [Conditional("DEBUG")]
-        [Conditional("RELEASE")]
-        public static void RunProgram()
-        {
-            // Infinite loop for the calculator menu
             while (true)
             {
-                // Display menu options
+                // Show menu
                 Console.WriteLine("[Menu] |-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-|");
                 Console.WriteLine("[Menu] | You can use this Simple Calculator for:               |");
                 Console.WriteLine("[Menu] | ADDITION: Enter '+' to perform Addition.              |");
                 Console.WriteLine("[Menu] | SUBTRACTION: Enter '-' to perform Subtraction         |");
                 Console.WriteLine("[Menu] | MULTIPLICATION: Enter '*' to perform Multiplication   |");
                 Console.WriteLine("[Menu] | DIVISION: Enter '/' to perform Division               |");
-                Console.WriteLine("[Menu] | FACTOR: Enter 'factor' to run factorization algorithm |");
                 Console.WriteLine("[Menu] | EXIT: Enter 'exit' to exit the calculator             |");
                 Console.WriteLine("[Menu] |-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-|");
 
-                // Take user input
+                // Take Response.
                 Console.Write("[Input]: ");
                 string? userInput = Console.ReadLine();
-
-                // Handle cases where user input is null or empty
-                if (userInput == null || userInput == "")
+                // User doesn't pass anything.
+                if (userInput == null || userInput=="")
                 {
                     Console.WriteLine("[Error]: Enter a valid choice.");
                     continue;
                 }
-
-                // Check if the user wants to exit
+                // Asked to exit.
                 if (userInput.ToLower() == "exit")
                 {
                     Console.WriteLine("[Exit] Exiting the calculator. Goodbye!");
                     break;
                 }
 
-
-                if (userInput.ToLower() == "factor")
-                {
-                    // Factorization option
-                    Console.Write("[Number]: Enter the number to factorize: ");
-                    if (!int.TryParse(Console.ReadLine(), out int numberToFactorize))
-                    {
-                        Console.WriteLine("[Error] Invalid input for factorization. Please enter a valid integer.");
-                        continue;
-                    }
-
-                    var factors = Core.OptimizedFactorAlgorithm(numberToFactorize);
-                    Console.WriteLine($"[Result] Factors of {numberToFactorize}: {string.Join(", ", factors)}"); 
-                    var factors2 = Core.SimpleFactorAlgorithm(numberToFactorize);
-                    Console.WriteLine($"[Result] Factors of {numberToFactorize}: {string.Join(", ", factors2)}");
-                    continue;
-                }
-
-                // Take in numbers for the calculation
+                // Take in Numbers
+                double result;
                 Console.Write("[Number]: Enter the first number: ");
                 if (!double.TryParse(Console.ReadLine(), out double num1))
                 {
                     Console.WriteLine("[Error] Invalid input for the first number. Please enter a valid number.");
                 }
-
                 Console.Write("[Number]: Enter the second number: ");
                 if (!double.TryParse(Console.ReadLine(), out double num2))
                 {
                     Console.WriteLine("[Error] Invalid input for the second number. Please enter a valid number.");
                 }
 
-                double result;
-                // Perform the selected operation based on user input
                 switch (userInput)
                 {
                     case "+":
@@ -129,6 +79,9 @@ namespace Calculator
                         Console.WriteLine("[Error] Invalid operation. Please enter a valid operation.");
                         break;
                 }
+
+                //  =-=-=-=-=-=-
+
             }
         }
     }
